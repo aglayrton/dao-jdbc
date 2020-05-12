bf7e42a3a3dfce28b31f9005662380f83eb0423a
Aglayrton <aglayrtonjuliao@gmail.com>
2020-05-12 20:38:14 -0300

    Classe Vendedor criado como entidade

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Programa {
 	
@@ -8,5 +11,7 @@ public class Programa {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");  
 		System.out.println(obj);
+		Seller seller = new Seller(1, "Julião", "juliao@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 }
